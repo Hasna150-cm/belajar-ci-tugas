814004ca35d2c84420f3dda673bcd7b92383fd52
Hasna150-cm <hasnakhairunnisa30@gmail.com>
2025-07-09 23:29:53 +0700

    home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -53,10 +53,4 @@ class Home extends BaseController
 
         return view('v_profile', $data);
     }
-
-    public function penjualan()
-    {
-        $data['transactions'] = $this->transaction->findAll();
-        return view('v_penjualan', $data);
-    }
 }
